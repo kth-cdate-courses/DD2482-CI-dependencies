32269cf9eef6c5ccd3a55a298eb86b51cd326046
Hampus Hallkvist <hampus@hallkvist.org>
2024-09-03 14:49:08 +0200

    fix: Add background img

--- apps/backoffice/src/App.tsx
+++ apps/backoffice/src/App.tsx
@@ -2,11 +2,24 @@ import { Button } from 'antd';
 import { DateTime } from 'luxon';
 import { useState } from 'react';
 import './App.css';
+import background from './assets/Abstract HUD Control Panel.jpg';
 
 function App() {
   const [counter, setCounter] = useState(0);
   return (
     <>
+      <img
+        src={background}
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          zIndex: -1,
+          filter: 'brightness(0.2)',
+          width: '100%',
+          height: '100%',
+        }}
+      />
       <p
         style={{
           fontSize: '30px',
